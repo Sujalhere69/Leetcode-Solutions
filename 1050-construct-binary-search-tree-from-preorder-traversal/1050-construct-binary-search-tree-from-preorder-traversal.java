b3909ed8a8673a9ed0f9062ff50d17f175e59ecb
Sujalhere69 <151685619+Sujalhere69@users.noreply.github.com>
2025-01-31 23:29:49 +0530

    Time: 0 ms (100%), Space: 41.7 MB (70.24%) - LeetHub

--- 1050-construct-binary-search-tree-from-preorder-traversal/1050-construct-binary-search-tree-from-preorder-traversal.java
+++ 1050-construct-binary-search-tree-from-preorder-traversal/1050-construct-binary-search-tree-from-preorder-traversal.java
@@ -14,24 +14,19 @@
  * }
  */
 class Solution {
-    public TreeNode bstFromPreorder(int[] preorder) {
-     int[] inorder = Arrays.copyOf(preorder, preorder.length);
-Arrays.sort(inorder);
- HashMap<Integer,Integer> map = new HashMap<>();
- for(int i =0 ; i<inorder.length;i++){
-    map.put(inorder[i],i);
- }
- return Tree(preorder,0 ,preorder.length-1,inorder,0 , inorder.length-1,map);
+    public TreeNode insert(TreeNode root, int val){
+        if(root==null)
+            return new TreeNode(val);
+        if(root.val>val)
+            root.left = insert(root.left, val);
+        if(root.val<val)
+            root.right = insert(root.right, val);
+        return root;
     }
-    public TreeNode Tree(int [] preorder, int ps , int pe , int [] inorder,int is , int ie , HashMap<Integer,Integer> map){
-        if(ps> pe || is > ie){
-           return null; 
-        }
-        int index = map.get(preorder[ps]);
-        int numsLeft = index - is ;
-        TreeNode root = new TreeNode(preorder[ps]);
-        root.left = Tree(preorder,ps+1 , ps+numsLeft,inorder, is , index-1,map);
-        root.right = Tree(preorder,ps+numsLeft+1,pe ,inorder, index +1,ie,map);
+    public TreeNode bstFromPreorder(int[] preorder) {
+        TreeNode root= null;
+        for(int n: preorder)
+            root = insert(root, n);
         return root;
     }
 }
