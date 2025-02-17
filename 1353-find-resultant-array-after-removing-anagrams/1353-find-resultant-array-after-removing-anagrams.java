c821a264f1a50c788e45db5853514f5c6bcc424b
Sujalhere69 <151685619+Sujalhere69@users.noreply.github.com>
2025-02-18 00:10:14 +0530

    Time: 2 ms (94.37%), Space: 44.9 MB (31.68%) - LeetHub

--- 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.java
+++ 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.java
@@ -1,29 +1,17 @@
 class Solution {
     public List<String> removeAnagrams(String[] words) {
-        for(int i =words.length-1; i>0;i--){
-            if(words[i].length()!= words[i-1].length()){
-                continue;
-            }
-            char [] curr = words[i].toCharArray();
-            char [] prev = words[i-1].toCharArray();
-            Arrays.sort(curr);
-            Arrays.sort(prev);
-            boolean flag = true;
-            for(int j =0 ; j<curr.length ; j++){
-                if(curr[j] != prev[j]){
-                    flag = false;
-                }
-            }
-            if(flag == true){
-                words[i]= " ";
+        ArrayList<String> result = new ArrayList<>();
+        String prevSorted = "";
+        for (int i = 0; i < words.length; i++) {
+            char[] arr = words[i].toCharArray();
+            Arrays.sort(arr);
+            String sortedWord = new String(arr);
+
+            if (!sortedWord.equals(prevSorted)) { 
+                result.add(words[i]);
+                prevSorted = sortedWord;
             }
         }
-        List<String> list = new ArrayList<>();
-        for(int i =0 ; i<words.length;i++){
-            if(words[i]!= " "){
-                list.add(words[i]);
-            }
-        }
-        return list;
+        return result;
     }
 }
