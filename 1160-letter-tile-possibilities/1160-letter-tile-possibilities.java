67a265366d63d2d6add9dd785a9e12f942d8f15c
Sujalhere69 <151685619+Sujalhere69@users.noreply.github.com>
2025-02-17 23:16:39 +0530

    Time: 2 ms (87.75%), Space: 41.3 MB (95.47%) - LeetHub

--- 1160-letter-tile-possibilities/1160-letter-tile-possibilities.java
+++ 1160-letter-tile-possibilities/1160-letter-tile-possibilities.java
@@ -1,19 +1,18 @@
 class Solution {
-    public int numTilePossibilities(String tiles) {
-        Set<String> sequences = new HashSet<>();
-        int len = tiles.length();
-        boolean [] used = new boolean [len];
-        generateSequences(tiles , "",used , sequences);
-        return sequences.size()-1;
-    }
-    public void generateSequences(String  tiles , String current , boolean [] used , Set<String> sequences){
-        sequences.add(current);
-        for(int pos =0 ; pos<tiles.length();pos++){
-            if(!used[pos]){
-                used[pos]=true;
-                generateSequences(tiles , current + tiles.charAt(pos),used , sequences);
-                used[pos]=false;
-            }
+    private int sequenceCount(int[] charCount) {
+        int totalCount=0;
+        for(int i=0;i<26;i++)   {
+            if(charCount[i]==0) continue;
+            totalCount++;
+            charCount[i]--;
+            totalCount+=sequenceCount(charCount);
+            charCount[i]++;
         }
+        return totalCount;
+    }
+    public int numTilePossibilities(String tiles) {
+        int[] charCount=new int[26];
+        for(char ch: tiles.toCharArray())   charCount[ch-'A']++;
+        return sequenceCount(charCount);
     }
 }
