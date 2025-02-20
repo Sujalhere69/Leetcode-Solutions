f7c4a1671b9c2cdac423ea0d57b3a5546abccb9c
Sujalhere69 <151685619+Sujalhere69@users.noreply.github.com>
2025-02-20 15:23:20 +0530

    Time: 9 ms (63.43%), Space: 45.8 MB (36.45%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.java
+++ 0214-shortest-palindrome/0214-shortest-palindrome.java
@@ -1,16 +1,40 @@
 class Solution {
     public String shortestPalindrome(String s) {
-        int i = 0;
-        int n = s.length();
-        for (int j = 0; j < n; j++) {
-            if (s.charAt(i) == s.charAt(n - j - 1)) {
-                i++;
+        if (s == null || s.length() == 0) return s;
+        
+        // Reverse the string
+        String rev = new StringBuilder(s).reverse().toString();
+        
+        // Create a new string with a separator
+        String combined = s + "#" + rev;
+        
+        // Compute the LPS array
+        int[] lps = computeLPS(combined);
+        
+        // Length of the longest palindromic prefix
+        int longestPalindromeLength = lps[combined.length() - 1];
+
+        // The suffix to be added (remaining part from the reversed string)
+        String suffixToAdd = rev.substring(0, s.length() - longestPalindromeLength);
+        
+        // Form the shortest palindrome
+        return suffixToAdd + s;
+    }
+
+    private int[] computeLPS(String pattern) {
+        int n = pattern.length();
+        int[] lps = new int[n];
+        int j = 0; // Length of the previous longest prefix suffix
+
+        for (int i = 1; i < n; i++) {
+            while (j > 0 && pattern.charAt(i) != pattern.charAt(j)) {
+                j = lps[j - 1]; // Move j to the previous LPS value
             }
-        } 
-        if (i == n) {
-            return s;
+            if (pattern.charAt(i) == pattern.charAt(j)) {
+                j++;
+            }
+            lps[i] = j;
         }
-        String p = new StringBuilder(s.substring(i, n)).reverse().toString();
-        return p + shortestPalindrome(s.substring(0, i)) + s.substring(i);
+        return lps;
     }
-    }
+}
