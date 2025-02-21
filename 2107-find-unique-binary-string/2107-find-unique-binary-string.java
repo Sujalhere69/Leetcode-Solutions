3eede78097770b309b6a92139cb19e7491924803
Sujalhere69 <151685619+Sujalhere69@users.noreply.github.com>
2025-02-22 02:20:23 +0530

    Time: 0 ms (100%), Space: 41.6 MB (64.34%) - LeetHub

--- 2107-find-unique-binary-string/2107-find-unique-binary-string.java
+++ 2107-find-unique-binary-string/2107-find-unique-binary-string.java
@@ -1,35 +1,10 @@
-import java.util.*;
-
 class Solution {
-    public String findDifferentBinaryString(String[] num) {
-        int n = num.length;
-        Set<String> set = new HashSet<>(Arrays.asList(num));
-        List<String> list = new ArrayList<>();
-
-        backTrack(0, new StringBuilder(), n, list);
-
-        for (String x : list) {
-            if (!set.contains(x)) {
-                return x;
-            }
+    public String findDifferentBinaryString(String[] nums) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < nums.length; i++) {
+            // Flip the i-th bit of nums[i] to ensure uniqueness
+            sb.append(nums[i].charAt(i) == '0' ? '1' : '0');
         }
-        return "";
-    }
-
-    public void backTrack(int index, StringBuilder st, int n, List<String> list) {
-        if (index == n) {
-            list.add(st.toString());
-            return;
-        }
-        
-        // Append '0'
-        st.append('0');
-        backTrack(index + 1, st, n, list);
-        st.deleteCharAt(st.length() - 1);
-
-        // Append '1'
-        st.append('1');
-        backTrack(index + 1, st, n, list);
-        st.deleteCharAt(st.length() - 1);
+        return sb.toString();
     }
 }
